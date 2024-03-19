bf4172cc74dca1e36afdd06d626a201473bc5c3c
alioshja <laboucheriedu12cestbeau@gmail.com>
2024-03-19 10:34:41 +0100

    eval-app-web 19/03/24 10H34

--- back-end/php/gestion-demande-intervention.php
+++ back-end/php/gestion-demande-intervention.php
@@ -5,48 +5,50 @@ session_start();
 //récupération des champ de form
 
 //modalitées de mission qui sera inscrit separément dans une bdd 
-$titre = $_POST['title'];
-$but = $_POST['goal'];
-$paysMission = $_POST['mission-country'];
+$titre = htmlspecialchars(htmlentities(stripslashes($_POST['title'])));
+$but =  htmlspecialchars(htmlentities(stripslashes($_POST['goal'])));
+$paysMission =  htmlspecialchars(htmlentities(stripslashes($_POST['mission-country'])));
 $nomCode = $_POST['code-name'];
 
 
 //info contact ... dans une bdd diférente
-$nomContact = $_POST['contact-name'];
-$prenomContact = $_POST['contact-firstname'];
-$DateNaissanceContact = $_POST['contact-birth-date'];
-$NationnaliteContact = $_POST['contact-nationnality'];
+$nomContact =  htmlspecialchars(htmlentities(stripslashes($_POST['contact-name'])));
+$prenomContact =  htmlspecialchars(htmlentities(stripslashes($_POST['contact-firstname'])));
+$DateNaissanceContact =  htmlspecialchars(htmlentities(stripslashes($_POST['contact-birth-date'])));
+$NationnaliteContact =  htmlspecialchars(htmlentities(stripslashes($_POST['contact-nationnality'])));
 
 
 //info cible ... dans une bdd diférente
-$nom = $_POST['name'];
-$prenom = $_POST['firstname'];
-$dateDeNaissance = $_POST['birth-date'];
-$Nationnalite = $_POST['nationnality'];
+$nom =  htmlspecialchars(htmlentities(stripslashes($_POST['name'])));
+$prenom = htmlspecialchars(htmlentities(stripslashes($_POST['firstname'])));
+$dateDeNaissance = htmlspecialchars(htmlentities(stripslashes($_POST['birth-date'])));
+$Nationnalite = htmlspecialchars(htmlentities(stripslashes($_POST['nationnality'])));
 
 //implementation table mission
-$sql1 = 'INSERT INTO missions ("titre, but, paysmission, nomdecodemission, userid") VALUE (":titre", ":but", ":paysmission", ":nomdecodemission", ":id")';
+$sql1 = 'INSERT INTO missions (titre, but, paysmission, nomdecodemission, userid) VALUE (:titre, :but, :paysmission, :nomdecodemission, :id)';
 $pdo1 = $bdd->prepare($sql1);
-$pdo1->bindParam($titre, ':titre');
-$pdo1->bindParam($but, ':but');
-$pdo1->bindParam($paysMission, ':paysmission');
-$pdo1->bindParam($nomCode, ':nomdecodemission');
-$pdo1->bindParam($_SESSION['id'], ':id');
+$pdo1->bindParam(':titre', $titre);
+$pdo1->bindParam(':but', $but);
+$pdo1->bindParam(':paysmission', $paysMission);
+$pdo1->bindParam(':nomdecodemission', $nomCode);
+$pdo1->bindParam(':id', $_SESSION['id']);
+$pdo1->execute();
 //implementation table contact
-$sql2 = 'INSERT INTO contact ("nomcontact, prenomcontact, datenaissancecontact, nationnalitecontact, userid") VALUE (":titre", ":but", ":paysmission", ":nomdecodemission", ":id")';
+$sql2 = 'INSERT INTO contact (nomcontact, prenomcontact, datenaissancecontact, nationnalitecontact, userid) VALUE (:titre, :but, :paysmission, :nomdecodemission, :id)';
 $pdo2 = $bdd->prepare($sql2);
-$pdo2->bindParam($nomContact, ':nomcontact');
-$pdo2->bindParam($prenomContact, ':prenomcontact');
-$pdo2->bindParam($DateNaissanceContact, ':datenaissancecontact');
-$pdo2->bindParam($NationnaliteContact, ':nationnalitecontact');
-$pdo2->bindParam($_SESSION['id'], ':id');
-
+$pdo2->bindParam(':nomcontact', $nomContact);
+$pdo2->bindParam(':prenomcontact', $prenomContact);
+$pdo2->bindParam(':datenaissancecontact', $DateNaissanceContact);
+$pdo2->bindParam(':nationnalitecontact', $NationnaliteContact);
+$pdo2->bindParam(':id', $_SESSION['id']);
+$pdo2->execute();
 //implementation table cible
-$sql3 = 'INSERT INTO cible ("nomcible, prenomcible, datenaissancecible, nationnalitecible, userid") VALUE (":titre", ":but", ":paysmission", ":nomdecodemission", ":id")';
+$sql3 = 'INSERT INTO cible (nomcible, prenomcible, datenaissancecible, nationnalitecible, userid) VALUE (:titre, :but, :paysmission, :nomdecodemission, :id)';
 $pdo3 = $bdd->prepare($sql3);
-$pdo3->bindParam($nom, ':nomcible');
-$pdo3->bindParam($prenom, ':prenomcible');
-$pdo3->bindParam($DateDeNaissance, ':datenaissancecible');
-$pdo3->bindParam($Nationnalite, ':nationnalitecible');
-$pdo3->bindParam($_SESSION['id'], ':id');
+$pdo3->bindParam(':nomcible', $nom);
+$pdo3->bindParam(':prenomcible', $prenom);
+$pdo3->bindParam(':datenaissancecible', $DateDeNaissance);
+$pdo3->bindParam(':nationnalitecible', $Nationnalite);
+$pdo3->bindParam(':id', $_SESSION['id']);
+$pdo3->execute();
 ?>
